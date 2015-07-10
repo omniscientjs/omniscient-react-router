412c0ff855d54256d0a59f4845581217bbef74d5
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-07-10 22:56:57 +0200

    Better show how state updates trigger re-renders and where overriding shouldComponentUpdate is nescessary when components don't receive changed props.

--- index.js
+++ index.js
@@ -4,34 +4,68 @@ var { Route, RouteHandler, DefaultRoute, Link } = Router;
 
 var RouterHelper = require('./router-helper');
 
-var component = require('omniscient').withDefaults({
-  jsx: true
-});
+var component = require('omniscient').withDefaults({ jsx: true });
+component.debug();
+
 var immstruct = require('immstruct');
 
 var data = immstruct({ even: 0, odd: 1 });
-setInterval(_ => data.cursor().update('even', m => m + 2), 100);
-setInterval(_ => data.cursor().update('odd',  n => n + 2), 100);
 
-var App = component(function () {
+var evenInterval, oddInterval;
+function toggleUpdates () {
+  if (evenInterval || oddInterval) {
+    clearInterval(evenInterval);
+    clearInterval(oddInterval);
+    evenInterval = null;
+    oddInterval = null;
+  }
+  else {
+    evenInterval = setInterval(_ => data.cursor().update('even', inc), 100);
+    oddInterval = setInterval(_ => data.cursor().update('odd', inc), 100);
+  }
+}
+
+var alwaysRerender = { shouldComponentUpdate: () => true };
+
+var App = component('App', [alwaysRerender], function () {
   return (
     <div>
       <ul>
         <li><Link to="even">Even</Link></li>
         <li><Link to="odd">Odd</Link></li>
+        <li><Link to="nested-route">Nested route</Link></li>
       </ul>
+      <button onClick={toggleUpdates}>toggle automatic updates</button>
       <RouteHandler {...this.props}/>
     </div>
   );
 });
 
-var Even = component(function ({ cursor }) {
-  return <p>Even {cursor.deref()}</p>
+var Even = component('Even', function ({ cursor }) {
+  return <p>Even {cursor.deref()} <button onClick={ _ => cursor.update(inc)}>increase</button></p>;
 });
 
-var Odd = component(function ({ cursor, statics }) {
+var Odd = component('Odd', function ({ cursor, statics }) {
   console.log('the route is', statics.path);
-  return <p>Odd {cursor.deref()}</p>
+  return <p>Odd {cursor.deref()} <button onClick={ _ => cursor.update(inc)}>increase</button></p>;
+});
+
+var OuterRoute = component('Outer', [alwaysRerender], function () {
+  return <div>
+    <p>Outer route</p>
+    <ul>
+      <li><Link to="nested-route">Nested route</Link></li>
+      <li><Link to="another-nested-route">Another nested route</Link></li>
+    </ul>
+    <RouteHandler />
+  </div>;
+});
+
+var NestedRoute = component('Nested', function () {
+  return <p>Nested route was rendered on {String(new Date())}</p>;
+});
+var AnotherNestedRoute = component('AnotherNested', function () {
+  return <p>Another nested route was rendered on {String(new Date())}</p>;
 });
 
 var RouterEven = RouterHelper(Even, data, 'even');
@@ -42,9 +76,17 @@ var routes = (
     <DefaultRoute handler={RouterEven} />
     <Route name="even" handler={RouterEven} />
     <Route name="odd" handler={RouterOdd} />
+    <Route name="outer-route" handler={OuterRoute}>
+      <Route name="nested-route" handler={NestedRoute}/>
+      <Route name="another-nested-route" handler={AnotherNestedRoute}/>
+    </Route>
   </Route>
 );
 
 Router.run(routes, function (Handler, state) {
   React.render(<Handler cursor={data.cursor()} statics={state} />, document.body);
 });
+
+function inc (m) {
+  return m + 2;
+}
